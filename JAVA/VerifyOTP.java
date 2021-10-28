7d79a7fbacb8caf853569014e0f9fe2c9d509ae8
Rishabh Sahni <rishabhsahni2001@gmail.com>
2021-10-28 21:25:36 +0530

    VerifyOTP

---
+++ JAVA/VerifyOTP.java
@@ -0,0 +1,10 @@
+public class VerifyOTP {
+
+    public static void main(String args[]){
+        int a,b,sum;
+        a = 6;
+        b= 6;
+        sum = a +b;
+        System.out.println(sum);
+    }
+}
